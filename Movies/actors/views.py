3969f8e1637708e6b0e6d1e279bd19145acea773
Abdulelah Alhussain <85552737+Abdulelah81@users.noreply.github.com>
2022-09-11 21:04:09 +0300

    Add files via upload

---
+++ Movies/actors/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+
+
+def top_actor(request : HttpRequest):
+    top_actor : str = "Denzel Washington"
+
+    return HttpResponse(f"The top actor in the world is {top_actor}")
